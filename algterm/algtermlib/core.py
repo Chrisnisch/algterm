f4b549c8771139df8c8f470b7148d69a2838688d
Mikhail Gusev <gusev2.mm@edu.spbstu.ru>
2024-12-15 21:23:13 +0300

    basic functionality done

--- algterm/algtermlib/core.py
+++ algterm/algtermlib/core.py
@@ -1,5 +1,6 @@
 import copy
 import re
+import sympy
 
 
 class Term:
@@ -20,8 +21,6 @@ class Term:
     def __set_arity(self, n):
         if n < 0:
             raise ValueError('Arity must be non-negative')
-        if self.arity > 0:
-            raise Warning('Setting arity 0 to a function')
         self.arity = n
 
     def set_symbol(self, s: str):
@@ -72,46 +71,74 @@ class Term:
         return f'{self.symbol}'
 
     def __add__(self, other):
-        return Expression(f"{self} + {other}")
+        return Expression(self, other, operator='+')
 
     def __mul__(self, other):
-        return Expression(f"{self} * {other}")
+        return Expression(self, other, operator='*')
+
+    def __truediv__(self, other):
+        return Expression(self, other, operator='/')
 
     def __pow__(self, power):
-        return Expression(f"{self}^{power}")
+        return Expression(self, power, operator='^')
 
 
 class Expression:
-    """ +-full GPTs code """
-    def __init__(self, value):
+    def __init__(self, left, right=None, operator=None):
         """
-        Initialize expression.
+        Initialize an expression.
 
-        :param value: Term or other expression.
+        :param left: Left operand (Term or Expression).
+        :param right: Right operand (Term, Expression, or None for unary operators).
+        :param operator: Operator as a string ('+', '*', '**', etc.).
         """
-        if isinstance(value, (Term, Expression, int, float, str)):
-            self.value = value
-        else:
-            raise TypeError('Expression must be initialized with Term, Expression, int, float, or str')
+        self.left = left
+        self.right = right
+        self.operator = operator
 
     def __add__(self, other):
-        return Expression(f"({self.value} + {other})")
+        return Expression(self, other, operator='+')
 
     def __mul__(self, other):
-        return Expression(f"{self.value} * {other}")
+        return Expression(self, other, operator='*')
+
+    def __truediv__(self, other):
+        return Expression(self, other, operator='/')
 
     def __pow__(self, power):
-        return Expression(f"({self.value})^{power}")
+        return Expression(self, power, operator='^')
 
     def __repr__(self):
-        return str(self.value)
+        """
+        Generate a string representation of the expression with minimal parentheses.
+
+        :return: String representation of the expression.
+        """
+        if not self.operator:
+            return str(self.left)
+
+        # Define operator precedence
+        precedence = {'+': 1, '*': 2, '/': 2, '^': 3}
+
+        # Get the precedence of the current operator
+        current_precedence = precedence[self.operator]
+
+        # Determine whether to wrap left and right operands in parentheses
+        left_repr = (
+            f"({self.left})" if isinstance(self.left, Expression) and precedence.get(self.left.operator, 0) < current_precedence else f"{self.left}"
+        )
+        right_repr = (
+            f"({self.right})" if isinstance(self.right, Expression) and precedence.get(self.right.operator, 0) <= current_precedence else f"{self.right}"
+        )
+
+        return f"{left_repr} {self.operator} {right_repr}"
 
 
-if __name__ == '__main__':
-    pass
-    # func_f = Term('f(x, y, α)', expr=(x ** 2 + y ** 2 + alpha))  # терм-функция f
-    # print(func_f)  # вывод терма-функции: f(x, y, α)
-    # print(func_f.expression)  # вывод выражения терма-функции: x^2 + y^2 + α
-    #
-    # expr = x + func_f.expression ** 2 + alpha  # выражение с термами
-    # print(expr)  # выводится полученное выражение: x + (x^2 + y^2 + α)^2 + α
+def simp(expr):
+    """
+    Simplify the expression using sympy.ratsimp().
+
+    :param expr: expression to simplify
+    :return: simplified expression
+    """
+    return str(sympy.ratsimp(str(expr))).replace("**", "^")
--- algterm/tests.py
+++ algterm/tests.py
@@ -1,3 +1,5 @@
+from algtermlib.algterm import Term, simp
 """
-write your test here
+write your tests here
 """
+
